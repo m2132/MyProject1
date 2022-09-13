7fcfcc8d7cc03a89ff6e513b2853dae9e20986ce
michalAdmin <112867735+michalAdmin@users.noreply.github.com>
2022-09-13 21:36:15 +0300

    add

--- MyProject.ConsoleApp/Program.cs
+++ MyProject.ConsoleApp/Program.cs
@@ -136,6 +136,9 @@ namespace MyProject.ConsoleApp
                 }
             }
             while (obj != "exit");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
         }
     }
 }
